9e1e9aa819fc97d564f1d90334673742486056e7
Will Woods Ballard <willwoodsb@gmail.com>
2023-03-07 16:14:10 +0000

    added login logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,8 +15,19 @@
                 </a>
             </div>
 
-            <div class="mt-8 md:mt-0">
-                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+            <div class="mt-8 md:mt-0 flex items-center">
+                @auth
+                    <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}</span>
+
+                    <form method="POST" action="/logout" class="text-xs font-smibold text-blue-500 ml-6">
+                        @csrf
+                        <button type="submit">Log Out</button>
+                    </form>
+
+                @else
+                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+                @endauth
 
                 <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
@@ -54,4 +65,14 @@
             </div>
         </footer>
     </section>
+
+    @if (session()->has('success'))
+        <div x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 4000)"
+            x-show="show"
+            class="fixed bottom-3 right-3 bg-blue-500 text-white py-2 px-4 rounded-xl text-sm"
+        >
+            <p>{{ session()->get('success') }}</p>
+        </div>
+    @endif
 </body>
